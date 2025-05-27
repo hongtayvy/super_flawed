3faabb5074de9585a50a143c00900ecd7abdba89
Victor Yang <victoryang@Victors-M1Pro-3.local>
2025-05-27 01:48:28 -0500

    chore: clean up cors

--- src/socket.ts
+++ src/socket.ts
@@ -1,11 +1,13 @@
 import { io } from 'socket.io-client';
-const URL =
-  import.meta.env.PROD
-    ? 'https://superflawed-production.up.railway.app'
-    : 'http://localhost:4000';
+
+const URL = import.meta.env.PROD
+  // include port 8080 on prod
+  ? 'https://superflawed-production.up.railway.app:8080'
+  : 'http://localhost:4000';
 
 export const socket = io(URL, {
-  transports: ['websocket'],  // ← no more polling
+  transports: ['websocket'],  // WebSocket only
+  path: '/socket.io',         // defaults to /socket.io but safe to be explicit
 });
 
 socket.on('connect_error', (err) => {
